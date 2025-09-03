0fa5691c495f0288d32b44e40cbade83e541ec5e
Nicholas Lipari <nicholas.lipari@louisiana.edu>
2025-09-03 11:39:42 -0500

    Added simple s-ptr example with BankAccount

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
-//
-// Created by nicho on 9/3/2025.
-//
+#include <iostream>
+#include <memory>  // for std::unique_ptr, std::make_unique
 
-#include <cstdio>
+class BankAccount {
+public:
+    BankAccount(double initialBalance) : balance(initialBalance) {}
 
-int main(int argc, const char * argv[]) {
-    printf("Hello World\n");
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            std::cout << "Insufficient funds\n";
+        }
+    }
+
+    void printBalance() const {
+        std::cout << "Balance: $" << balance << "\n";
+    }
+
+private:
+    double balance;
+};
+
+int main() {
+    // Create a smart pointer that owns a BankAccount
+    std::unique_ptr<BankAccount> account = std::make_unique<BankAccount>(100.0);
+
+    account->deposit(50);
+    account->withdraw(30);
+    account->printBalance();
+
+
+    // No need to delete manually; memory is freed automatically
 }
